8d0871531f1f958dfe095172d5abec457379829c
Giovanny Ospina Alzate <giospin@bancolombia.com.co>
2024-08-20 16:21:39 -0500

    se sube el resultado de la practica 5

---
+++ ecommerce_app/lib/core/store.dart
@@ -0,0 +1,13 @@
+import 'package:velocity_x/velocity_x.dart';
+import '../models/cart.dart';
+import '../models/catelog.dart';
+
+class MyStore extends VxStore {
+  CatelogModel? catelog;
+  CartModel? cart;
+  MyStore() {
+    catelog = CatelogModel();
+    cart = CartModel();
+    cart!.catelog = catelog!;
+  }
+}
